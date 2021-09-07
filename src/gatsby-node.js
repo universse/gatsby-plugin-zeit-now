4023f50bae09ecdf7b22ba3429ce704769a71967
universse <phuoc317049@gmail.com>
2021-09-07 22:26:56 +0800

    Support wildcard path redirects

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,68 +1,76 @@
 const { writeFileSync } = require('fs')
 const { join } = require('path')
 
-// Do not change this.
-const REDIRECT_FILE_NAME = '__now_routes_g4t5bY.json'
-
-const security = [
-  {
-    src: '/.*',
-    headers: {
-      'referrer-policy': 'same-origin',
-      'feature-policy': "geolocation 'self'; microphone 'self'; camera 'self'",
-      'expect-ct': 'max-age=604800, enforce',
-      'strict-transport-security': 'max-age=31536000; includeSubDomains',
-      'x-frame-options': 'DENY',
-      'x-xss-protection': '1; mode=block',
-      'x-content-type-options': 'nosniff',
-      'x-download-options': 'noopen',
-    },
-    continue: true,
-  },
-]
-
-const alwaysCache = {
-  headers: { 'cache-control': 'public, max-age=31536000, immutable' },
-  continue: true,
-}
-
-const caching = [
-  {
-    src: '^/(icons|static)/(.*)$',
-    ...alwaysCache,
-  },
-  {
-    src: '^/.*\\.(js|css)$',
-    ...alwaysCache,
-  },
-  {
-    src: '^/(sw\\.js|app-data\\.json|.*\\.html|page-data/.*)$',
-    headers: { 'cache-control': 'public,max-age=0,must-revalidate' },
-    continue: true,
-  },
-]
-
-const filesystem = { handle: 'filesystem' }
-
-const notFound = { src: '/.*', status: 404, dest: '/404' }
-
-const localizedNotFound = []
-
 exports.onPostBuild = (
   { store },
   { globalHeaders = {}, headers = {}, redirects: nowRedirects = [] } = {}
 ) => {
+  // Do not change this.
+  const REDIRECT_FILE_NAME = '__now_routes_g4t5bY.json'
+
+  // cache-control headers
+  const ALWAYS_CACHE = {
+    headers: { 'cache-control': 'public, max-age=31536000, immutable' },
+    continue: true,
+  }
+
+  const NEVER_CACHE = {
+    headers: { 'cache-control': 'public,max-age=0,must-revalidate' },
+    continue: true,
+  }
+
+  // routes
+  const securityRoutes = [
+    {
+      src: '/.*',
+      headers: {
+        'referrer-policy': 'same-origin',
+        'feature-policy':
+          "geolocation 'self'; microphone 'self'; camera 'self'",
+        'expect-ct': 'max-age=604800, enforce',
+        'strict-transport-security': 'max-age=31536000; includeSubDomains',
+        'x-frame-options': 'DENY',
+        'x-xss-protection': '1; mode=block',
+        'x-content-type-options': 'nosniff',
+        'x-download-options': 'noopen',
+      },
+      continue: true,
+    },
+  ]
+
+  const cachingRoutes = [
+    {
+      src: '^/(icons|static)/(.*)$',
+      ...ALWAYS_CACHE,
+    },
+    {
+      src: '^/.*\\.(js|css)$',
+      ...ALWAYS_CACHE,
+    },
+    {
+      src: '^/(sw\\.js|app-data\\.json|.*\\.html|page-data/.*)$',
+      ...NEVER_CACHE,
+    },
+  ]
+
+  const preFilesystemRoutes = []
+
+  const filesystemRoute = { handle: 'filesystem' }
+
+  const postFilesystemRoutes = []
+
+  const localizedNotFoundRoutes = []
+
+  const notFoundRoute = { src: '/.*', status: 404, dest: '/404' }
+
   const {
     pages,
     program: { directory },
     redirects,
   } = store.getState()
 
-  const pre = []
-  const post = []
-
   nowRedirects.forEach(({ source, destination, statusCode = 308 }) => {
-    post.push({
+    postFilesystemRoutes.push({
       src: source,
       status: statusCode,
       headers: { Location: destination },
@@ -70,23 +78,40 @@ exports.onPostBuild = (
   })
 
   // redirects
+  const SPLAT_REGEXP = /\*/
+
   redirects.forEach(({ fromPath, toPath, force, isPermanent, statusCode }) => {
     let status = isPermanent ? 301 : 302
     status = statusCode || status
 
+    let src = fromPath
+    let dest = toPath
+
+    const isSrcWildcard = SPLAT_REGEXP.test(src)
+    const isDestWildcard = SPLAT_REGEXP.test(dest)
+
+    if (isSrcWildcard && isDestWildcard) {
+      src = src.replace(SPLAT_REGEXP, '(.*)')
+      dest = dest.replace(SPLAT_REGEXP, '$1')
+    }
+
+    if (isSrcWildcard && !isDestWildcard) {
+      src = src.replace(SPLAT_REGEXP, '.*')
+    }
+
     const route =
       status === 200
         ? {
-            src: fromPath,
-            dest: toPath,
+            src,
+            dest,
           }
         : {
-            src: fromPath,
+            src,
             status,
-            headers: { Location: toPath },
+            headers: { Location: dest },
           }
 
-    force ? pre.push(route) : post.push(route)
+    force ? preFilesystemRoutes.push(route) : postFilesystemRoutes.push(route)
   })
 
   // client-only routes
@@ -94,31 +119,31 @@ exports.onPostBuild = (
   const NOT_FOUND_REGEXP = /\/404\/?$/
 
   Array.from(pages.values())
-    .filter(page => page.matchPath && page.matchPath !== page.path)
-    .forEach(page => {
+    .filter((page) => page.matchPath && page.matchPath !== page.path)
+    .forEach((page) => {
       const src = page.matchPath.replace(GATSBY_MATCHPATH_REGEXP, '.*')
       const dest = page.path
 
       const isNotFoundPage = NOT_FOUND_REGEXP.test(dest)
 
       isNotFoundPage
-        ? localizedNotFound.push({
+        ? localizedNotFoundRoutes.push({
             src,
             status: 404,
             dest,
           })
-        : pre.push({
+        : preFilesystemRoutes.push({
             src,
             dest,
           })
     })
 
   // merge globalHeaders from pluginOptions
-  security[0].headers = { ...security[0].headers, ...globalHeaders }
+  securityRoutes[0].headers = { ...securityRoutes[0].headers, ...globalHeaders }
 
   // add per-route headers from pluginOptions
   Object.entries(headers).forEach(([route, routeHeaders]) => {
-    security.push({
+    securityRoutes.push({
       src: route,
       headers: routeHeaders,
       continue: true,
@@ -126,13 +151,13 @@ exports.onPostBuild = (
   })
 
   const routes = [
-    ...security,
-    ...caching,
-    ...pre,
-    filesystem,
-    ...post,
-    ...localizedNotFound,
-    notFound,
+    ...securityRoutes,
+    ...cachingRoutes,
+    ...preFilesystemRoutes,
+    filesystemRoute,
+    ...postFilesystemRoutes,
+    ...localizedNotFoundRoutes,
+    notFoundRoute,
   ]
 
   writeFileSync(
